b926f63ce03259507a3af946e78b54862ef37c40
mpvii <mrkprtr@hotmail.co.uk>
2024-11-14 14:31:54 +0000

    fixing routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
+
 
 function App() {
     return (
-        <Router>
+        <Router >
             <div className="App">
                 <Routes>
                     <Route path="/" element={<Home />} />
